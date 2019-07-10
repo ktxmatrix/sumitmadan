dc27e56ae0737f9a8a73decb1c7b06fb11984d37
ktxmatrix <>
2019-07-09 23:35:33 -0400

    First commit of python webapp

---
+++ script1.py
@@ -0,0 +1,18 @@
+# Importing the Flask object from the flask library
+from flask import Flask, render_template
+
+# Instantiating the Flask class using the app object
+app=Flask(__name__)
+# __name__ is a special variable you get for the name of the python script
+
+@app.route('/')
+def home(): # defines what our web home page will do
+    return render_template("home.html")
+
+@app.route('/about/')
+def about(): # defines what our web home page will do
+    return render_template("about.html")
+
+# When you execite the script, python assigns the name __main__
+if __name__ == "__main__":
+    app.run(debug=True) # Would not be executed if the script was imported from somewhere else
